8bb3ec81ee257efc396876fef7da1db9d03281cd
Guillaume <71400691+katsutsani@users.noreply.github.com>
2023-11-16 17:31:56 +0100

    p

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -1,75 +1,75 @@
-#include "Game.h"
-#include "Menu.h"
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-
-
-int WinMain() {
-    RenderWindow window(sf::VideoMode(960, 960), "Main Menu", Style::Default);
-    Menu mainMenu;
-
-
-    while (window.isOpen())
-    {
-        window.clear();
-        Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
-            {
-                window.close();
-            }
-            if (event.type == Event::KeyReleased)
-            {
-                if (event.key.code == Keyboard::Up)
-                {
-                    mainMenu.MoveUp();
-                    break;
-                }
-
-                if (event.key.code == Keyboard::Down)
-                {
-                    mainMenu.MoveDown();
-                    break;
-                }
-
-                if (event.key.code == Keyboard::Return)
-                {
-
-
-
-                    int x = mainMenu.MenuPressed();
-                    if (x == 0)
-                    {
-                        window.close();
-                        sf::RenderWindow Gamewindow(sf::VideoMode(WINDOW_SIZE, GRID_SIZE), "Tic Tac Toe");
-
-                        Game game;
-
-                        while (Gamewindow.isOpen()) {
-                            game.Update(Gamewindow);
-                            game.Render(Gamewindow);
-                        }
-                    }
-
-                    if (x == 1)
-                    {
-
-                    }
-                    if (x == 2)
-                    {
-                        window.close();
-                        break;
-                    }
-
-                }
-            }
-        }
-
-        mainMenu.draw(window);
-        window.display();
-    }
-
-    return 0;
-}
+//#include "Game.h"
+//#include "Menu.h"
+//#include <iostream>
+//#include <SFML/Graphics.hpp>
+//#include <SFML/Audio.hpp>
+//
+//
+//int WinMain() {
+//    RenderWindow window(sf::VideoMode(960, 960), "Main Menu", Style::Default);
+//    Menu mainMenu;
+//
+//
+//    while (window.isOpen())
+//    {
+//        window.clear();
+//        Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == Event::Closed)
+//            {
+//                window.close();
+//            }
+//            if (event.type == Event::KeyReleased)
+//            {
+//                if (event.key.code == Keyboard::Up)
+//                {
+//                    mainMenu.MoveUp();
+//                    break;
+//                }
+//
+//                if (event.key.code == Keyboard::Down)
+//                {
+//                    mainMenu.MoveDown();
+//                    break;
+//                }
+//
+//                if (event.key.code == Keyboard::Return)
+//                {
+//
+//
+//
+//                    int x = mainMenu.MenuPressed();
+//                    if (x == 0)
+//                    {
+//                        window.close();
+//                        sf::RenderWindow Gamewindow(sf::VideoMode(WINDOW_SIZE, GRID_SIZE), "Tic Tac Toe");
+//
+//                        Game game;
+//
+//                        while (Gamewindow.isOpen()) {
+//                            game.Update(Gamewindow);
+//                            game.Render(Gamewindow);
+//                        }
+//                    }
+//
+//                    if (x == 1)
+//                    {
+//
+//                    }
+//                    if (x == 2)
+//                    {
+//                        window.close();
+//                        break;
+//                    }
+//
+//                }
+//            }
+//        }
+//
+//        mainMenu.draw(window);
+//        window.display();
+//    }
+//
+//    return 0;
+//}
